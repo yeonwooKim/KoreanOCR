704048badaf241410fe45dcf2522b59de538f80f
yeonwooKim <nokjiwon96@gmail.com>
2016-10-08 23:41:01 +0900

    Output line images in the right form
    * Read comment for more information

--- Detection/Detect_line.py
+++ Detection/Detect_line.py
@@ -15,7 +15,7 @@ grayscale_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
 def sumup_column(img, row_number):
 	sum = 0
 	(_, length) = img.shape
-	for i in range(0, length):
+	for i in range (0, length):
 		sum = sum + img.item(row_number, i)
 	return sum / length
 
@@ -23,7 +23,7 @@ def sumup_column(img, row_number):
 def sumup_row(img, col_number):
 	sum = 0
 	(length, _) = img.shape
-	for i in range(0, length):
+	for i in range (0, length):
 		sum = sum + img.item(i, col_number)
 	return sum / length
 
@@ -55,6 +55,7 @@ def find_line(img):
 
 	return trunc_row
 
+# implemented for testing; returns list of paragraph and line #
 def label_location(trunc_row):
 	label, num_p, num_l = [], 0, 0
 	length = len(trunc_row)
@@ -66,6 +67,18 @@ def label_location(trunc_row):
 			num_l = 0
 	label.append((num_p, num_l))
 	return label
+
+# returns pair of total number of paragraphs and 
+#list of labels containing paragraph #
+def label_paragraph(trunc_row):
+	label, num = [], 0
+	length = len(trunc_row)
+	for i in range (0, length - 1):
+		label.append(num)
+		if trunc_row[i + 1][0] - trunc_row[i][1] > 20:
+			num = num + 1
+	label.append(num)
+	return (num + 1, label)
 
 # trims line images
 def trim_line(img):
@@ -98,12 +111,26 @@ def get_line_imgs(img, trunc_row):
 		line_imgs.append(trim_line(img[trunc_row[i][0]:trunc_row[i][1]]))
 	return line_imgs
 
+# implemented for testing; saves line images by paragraph and line number
 def save_line_imgs(img, trunc_row):
-	length = len (trunc_row)
+	length = len(trunc_row)
 	label = label_location(trunc_row)
 	imgs = get_line_imgs(img, trunc_row)
 	for i in range (0, length):
 		name = "test/paragraph" + str(label[i][0]) + "line" + str(label[i][1]) + ".png"
 		cv2.imwrite(name, imgs[i])
 
-save_line_imgs(im_bw, find_line(im_bw))
+# outputs line imgs in the form of following:
+# [paragraph_1, paragraph_2, ...]
+# paragraph_n = [line_1, line_2, ...]
+# line_n = line img
+def output_line_imgs(img, trunc_row):
+	essay = []
+	length = len (trunc_row)
+	(num_p, label) = label_paragraph(trunc_row)
+	for i in range (0, num_p):
+		essay.append([])
+	imgs = get_line_imgs(img, trunc_row)	
+	for i in range (0, length):
+		essay[label[i]].append(imgs[i])
+	return essay
